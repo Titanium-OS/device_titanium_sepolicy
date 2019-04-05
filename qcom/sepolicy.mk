c93a459eb671a93bf72a96ddb154b0021c684830
SonalSingh18 <sonal.singh.19993@gmail.com>
2019-04-05 16:38:53 +0530

    Its Titanium Time...

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Titanium
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,13 +10,13 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/titanium/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/dynamic
+    device/titanium/sepolicy/qcom/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/dynamic \
-    device/lineage/sepolicy/qcom/vendor
+    device/titanium/sepolicy/qcom/dynamic \
+    device/titanium/sepolicy/qcom/vendor
 endif

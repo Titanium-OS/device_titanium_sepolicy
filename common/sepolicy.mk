917211eeaad79ef2b8af830e2df73884308bcff4
Saikat Halder <iamdevsaikat@gmail.com>
2019-11-10 12:54:12 +0000

    sepolicy: it's Titanium time
    Change-Id: I9043dd479f6a58e6c9956e4b05545cd5c4dd773d

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/titanium/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/titanium/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/dynamic
+    device/titanium/sepolicy/common/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/titanium/sepolicy/common/dynamic \
+    device/titanium/sepolicy/common/vendor
 endif
